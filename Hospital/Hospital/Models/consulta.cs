a0e9f213dafddfd8f437c5c2c5c1e147814a9198
finalandr <20163524@itla.edu.do>
2018-12-03 09:57:30 -0400

    con_entity

---
+++ Hospital/Hospital/Models/consulta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital.Models
+{
+    public class consulta
+    {
+        public int idEmpleado { get; set; }
+        public int idPaciente { get; set; }
+        public DateTime fechaConsulta { get; set; }
+        public int costo { get; set; }
+        public string motivo { get; set; }
+    }
+}
